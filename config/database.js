75eb38fa0a7a746f92c7de3afbf5b60637c11ff8
siska anggita <122153487+agikik@users.noreply.github.com>
2024-07-24 18:31:01 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('e-learning-system', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
-  port: '3307'
+  port: '3306'
 });
 
 module.exports = sequelize;
